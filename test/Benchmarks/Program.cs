9f71d7381f2cd9f8577a2efd09fd045f26aee269
gholland@dataexpertsgroup.com <gholland@dataexpertsgroup.com>
2019-10-15 15:02:09 +1100

    fixed build

---
+++ test/Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // BenchmarkRunner.Run<Equals>();
+            // BenchmarkRunner.Run<AddInt>();
+            // BenchmarkRunner.Run<AddDecimal>();
+            BenchmarkRunner.Run<Assignment>();
+        }
+    }
+}
