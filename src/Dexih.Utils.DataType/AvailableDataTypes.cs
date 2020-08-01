c3ce0d46723a195b410da69cd963871e76676462
gholland@dataexpertsgroup.com <gholland@dataexpertsgroup.com>
2020-08-01 13:26:21 +1000

    Added more types fo AvailableDataTypes.

--- src/Dexih.Utils.DataType/AvailableDataTypes.cs
+++ src/Dexih.Utils.DataType/AvailableDataTypes.cs
@@ -1,24 +1,46 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Linq;
+using NetTopologySuite.Geometries.Implementation;
 
 namespace Dexih.Utils.DataType
 {
+    /// <summary>
+    /// Checks string values for potential datatypes and eliminates non-compliant values.
+    /// </summary>
     public struct AvailableDataTypes
     {
         private bool _valueChecked;
 
         public bool CharType { get; set; }
+        
+        public bool CharArrayType { get; set; }
+        public bool JsonType { get; set; }
+        public bool XmlType { get; set; }
+        public bool GuidType { get; set; }
+        public bool DateType { get; set; }
+        public bool TimeType { get; set; }
         public bool Int32Type { get; set; }
         public bool Int64Type { get; set; }
         public bool DoubleType { get; set; }
         public bool BooleanType { get; set; }
         public bool DateTimeType { get; set; }
-        public bool StringType { get; set; }
+
+        public int MinLength { get; set; }
+        public int MaxLength { get; set; }
 
         public void Reset()
         {
             _valueChecked = false;
         }
         
+        /// <summary>
+        /// Checks the value for different data types and eliminates the ones that are invalid.
+        /// Can be called multiple times to check values as needed.
+        /// </summary>
+        /// <param name="value"></param>
         public void CheckValue(string value)
         {
             if (value == null) return;
@@ -31,65 +53,147 @@ namespace Dexih.Utils.DataType
                 DoubleType = true;
                 BooleanType = true;
                 DateTimeType = true;
-                StringType = true;
+                JsonType = true;
+                XmlType = true;
+                GuidType = true;
+                DateType = true;
+                CharArrayType = true;
+                MinLength = Int32.MaxValue;
+                MaxLength = 0;
             }
 
             _valueChecked = true;
 
-            if (value.Length > 1)
+            if (CharType && value.Length > 1)
             {
                 CharType = false;
             }
 
-            if (!int.TryParse(value, out var _))
+            if (Int32Type && !int.TryParse(value, out _))
             {
                 Int32Type = false;
             }
 
-            if (!long.TryParse(value, out var _))
+            if (Int64Type && !long.TryParse(value, out _))
             {
                 Int64Type = false;
             }
 
-            if (!Operations.TryParseBoolean(value, out var _))
+            if (BooleanType && !Operations.TryParseBoolean(value, out _))
             {
                 BooleanType = false;
             }
 
-            if (!DateTime.TryParse(value, out var _))
+            if (DateTimeType && DateTime.TryParse(value, out var dateTime))
             {
-                DateTimeType = false;
-            }
-
-            if (!double.TryParse(value, out var _))
-            {
-                DoubleType = false;
+                if (dateTime.TimeOfDay != TimeSpan.Zero)
+                {
+                    DateType = false;
+                }
             }
             else
             {
-                // if the string is a double then override the datetime parse with a false (as value like 1.1 will incorrectly parse as a date)
                 DateTimeType = false;
+                DateType = false;
             }
 
-            if (value.Length > 4000)
+            if (TimeType && !TimeSpan.TryParse(value, out _))
             {
-                StringType = false;
+                TimeType = false;
             }
+
+            if (DateTimeType && !DateTime.TryParse(value, out _))
+            {
+                DateType = false;
+            }
+            
+            if (DoubleType)
+            {
+                if (!double.TryParse(value, out _))
+                {
+                    DoubleType = false;
+                }
+                else
+                {
+                    // if the string is a double then override the datetime parse with a false (as value like 1.1 will incorrectly parse as a date)
+                    DateTimeType = false;
+                }
+            }
+
+            if (JsonType)
+            {
+                try
+                {
+                    if(value.IndexOfAny(new []{'{', '['}) < 0)
+                    {
+                        JsonType = false;
+                    }
+                    JsonDocument.Parse(value);
+                }
+                catch
+                {
+                    JsonType = false;
+                }
+            }
+
+            if (XmlType)
+            {
+                try
+                {
+                    var doc = new XmlDocument();
+                    doc.LoadXml(value);
+                }
+                catch
+                {
+                    XmlType = false;
+                }
+            }
+
+            if (GuidType && !Guid.TryParse(value, out _))
+            {
+                GuidType = false;
+            }
+            
+
+            if (value.Length < MinLength)
+            {
+                MinLength = value.Length;
+            }
+
+            if (value.Length > MaxLength)
+            {
+                MaxLength = value.Length;
+            }
+
+            if (MinLength != MaxLength)
+            {
+                CharArrayType = false;
+            }
+
         }
 
+        /// <summary>
+        /// Get the most likely time by testing for non-string types, and then falling back to string if nothing else matches.
+        /// </summary>
+        /// <returns></returns>
         public ETypeCode GetBestType()
         {
             if (!_valueChecked) return ETypeCode.String;
 
             if (BooleanType) return ETypeCode.Boolean;
+            if (DateType) return ETypeCode.Date;
             if (DateTimeType) return ETypeCode.DateTime;
+            if (TimeType) return ETypeCode.Time;
             if (Int32Type) return ETypeCode.Int32;
             if (Int64Type) return ETypeCode.Int64;
             if (DoubleType) return ETypeCode.Double;
             if (CharType) return ETypeCode.Char;
-            if (StringType) return ETypeCode.String;
-
-            return ETypeCode.Text;
+            if (CharArrayType) return ETypeCode.CharArray;
+            if (JsonType) return ETypeCode.Json;
+            if (XmlType) return ETypeCode.Xml;
+            if (GuidType) return ETypeCode.Guid;
+            
+            return ETypeCode.String;
         }
     }
 }
--- test/Dexih.Utils.DataType.Tests/AvailableDataTypes.cs
+++ test/Dexih.Utils.DataType.Tests/AvailableDataTypes.cs
@@ -7,28 +7,34 @@ namespace Dexih.Utils.DataType.Tests
     public class TestAvailableDataTypes
     {
         [Theory]
-        [InlineData(new [] {"1.1", "1.2"}, new [] {TypeCode.Double, TypeCode.String})]
-        [InlineData(new [] {"1.1", "abc"}, new [] {TypeCode.String})]
-        [InlineData(new [] {"1 jan 2000", "2000/01/01"}, new [] {TypeCode.DateTime, TypeCode.String})]
-        [InlineData(new [] {"1234", "123456"}, new [] {TypeCode.Int32, TypeCode.Int64, TypeCode.Double, TypeCode.String})]
-        [InlineData(new [] {"6000000000", "7000000000"}, new [] {TypeCode.Int64, TypeCode.Double, TypeCode.String})]
-        [InlineData(new [] {"6000000000", "700.1"}, new [] {TypeCode.Double, TypeCode.String})]
-        [InlineData(new [] {"0", "1"}, new [] {TypeCode.Char, TypeCode.Int32, TypeCode.Int64, TypeCode.Double, TypeCode.Boolean, TypeCode.String})]
-        [InlineData(new [] {"a", "b"}, new [] {TypeCode.Char, TypeCode.String})]
-        [InlineData(new [] {"a", "bb"}, new [] {TypeCode.String})]
-        [InlineData(new [] {"yes", "no", "true", "false", "on", "off", "n", "y", "0", "1"}, new [] {TypeCode.Boolean, TypeCode.String})]
-        public void AvailableDataTypes(string[] values, TypeCode[] available)
+        [InlineData(new [] {"1.1", "1.2"}, new [] {ETypeCode.Double, ETypeCode.String})]
+        [InlineData(new [] {"1.1", "abc"}, new [] {ETypeCode.String})]
+        [InlineData(new [] {"1 jan 2000", "2000/01/01"}, new [] {ETypeCode.DateTime, ETypeCode.String})]
+        [InlineData(new [] {"1234", "123456"}, new [] {ETypeCode.Int32, ETypeCode.Int64, ETypeCode.Double, ETypeCode.String})]
+        [InlineData(new [] {"6000000000", "7000000000"}, new [] {ETypeCode.Int64, ETypeCode.Double, ETypeCode.String})]
+        [InlineData(new [] {"6000000000", "700.1"}, new [] {ETypeCode.Double, ETypeCode.String})]
+        [InlineData(new [] {"0", "1"}, new [] {ETypeCode.Char, ETypeCode.Int32, ETypeCode.Int64, ETypeCode.Double, ETypeCode.Boolean, ETypeCode.String})]
+        [InlineData(new [] {"a", "b"}, new [] {ETypeCode.Char, ETypeCode.String})]
+        [InlineData(new [] {"a", "bb"}, new [] {ETypeCode.String})]
+        [InlineData(new [] {"yes", "no", "true", "false", "on", "off", "n", "y", "0", "1"}, new [] {ETypeCode.Boolean, ETypeCode.String})]
+        [InlineData(new [] {"<xml><test></test></xml>"}, new [] {ETypeCode.Xml, ETypeCode.String})]
+        [InlineData(new [] {"[]", "{\"test\": 1}"}, new [] {ETypeCode.Json, ETypeCode.String})]
+        [InlineData(new [] {"751b03ec-1a67-4b84-8420-2b7a23ded33a"}, new [] {ETypeCode.Guid, ETypeCode.String})]
+        [InlineData(new [] {"abc", "def"}, new [] {ETypeCode.CharArray, ETypeCode.String})]
+        public void AvailableDataTypes(string[] values, ETypeCode[] available)
         {
             var avail = new AvailableDataTypes();
             foreach(var value in values) avail.CheckValue(value);
             
-            Assert.Equal(available.Contains(TypeCode.Char), avail.CharType);
-            Assert.Equal(available.Contains(TypeCode.Int32), avail.Int32Type);
-            Assert.Equal(available.Contains(TypeCode.Int64), avail.Int64Type);
-            Assert.Equal(available.Contains(TypeCode.Double), avail.DoubleType);
-            Assert.Equal(available.Contains(TypeCode.String), avail.StringType);
-            Assert.Equal(available.Contains(TypeCode.Boolean), avail.BooleanType);
-            Assert.Equal(available.Contains(TypeCode.DateTime), avail.DateTimeType);
+            Assert.Equal(available.Contains(ETypeCode.Char), avail.CharType);
+            Assert.Equal(available.Contains(ETypeCode.Int32), avail.Int32Type);
+            Assert.Equal(available.Contains(ETypeCode.Int64), avail.Int64Type);
+            Assert.Equal(available.Contains(ETypeCode.Double), avail.DoubleType);
+            Assert.Equal(available.Contains(ETypeCode.Boolean), avail.BooleanType);
+            Assert.Equal(available.Contains(ETypeCode.DateTime), avail.DateTimeType);
+            Assert.Equal(available.Contains(ETypeCode.Xml), avail.XmlType);
+            Assert.Equal(available.Contains(ETypeCode.Json), avail.JsonType);
+            Assert.Equal(available.Contains(ETypeCode.Guid), avail.GuidType);
             
         }
     }
